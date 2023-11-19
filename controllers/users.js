68e70ae39c51ee3c52a357aa5302359d61492d57
Rhilim <arhilim@gmail.com>
2023-11-19 13:44:27 -0600

    login done

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,11 @@ const registerSchema = Joi.object({
   password: Joi.string().required(),
 });
 
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
 async function register(req, res, next) {
   const { email, password } = req.body;
 
@@ -37,4 +42,37 @@ async function register(req, res, next) {
   }
 }
 
-module.exports = { register };
+async function login(req, res, next) {
+  const { email, password } = req.body;
+
+  try {
+    const validation = loginSchema.validate(
+      { email, password },
+      { abortEarly: false }
+    );
+
+    if (validation.error) {
+      return res.status(400).json({
+        message: `Validation error: ${validation.error.message}`,
+      });
+    }
+
+    const user = await User.findOne({ email: email }).exec();
+
+    if (user === null) {
+      return res.status(401).send({ message: "Email or password is wrong" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch === false) {
+      return res.status(401).send({ message: "Email or password is wrong" });
+    }
+
+    res.send({ token: "TOKEN" });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { register, login };
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,5 +7,6 @@ const router = express.Router();
 const jsonParser = express.json();
 
 router.post("/register", jsonParser, UsersController.register);
+router.post("/login", jsonParser, UsersController.login);
 
 module.exports = router;
